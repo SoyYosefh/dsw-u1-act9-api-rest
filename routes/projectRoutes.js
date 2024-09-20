8501c6dd51a66d77227d10349106504c5a2f9434
SoyYosefh <joratejedana@gmail.com>
2024-09-20 09:10:32 -0700

    fix de importacion

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const projectController = require('../controllers/taskControllers');
+const projectController = require('../controllers/projectControllers');
 
 //* modelo utilizado
 // {
