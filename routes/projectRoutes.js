53ad2a490830e1d7561264150b71761bea8c6ee3
SoyYosefh <joratejedana@gmail.com>
2024-09-20 08:43:17 -0700

    Se crean las rutas

---
+++ routes/projectRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /projects
+router.get('/getAll/', (req, res) => {});
+
+// POST /projects
+router.post('/create/', (req, res) => {});
+
+// PUT /projects/:id
+router.put('/update/:id', (req, res) => {});
+
+// DELETE /projects/:id
+router.delete('/delete/:id', (req, res) => {});
+
+// GET /projects/:id
+router.get('/get/:id', (req, res) => {});
